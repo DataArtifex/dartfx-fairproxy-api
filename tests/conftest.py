1a01f8b0f19a74aafa938a7979d1b462725809ee
kulnor <pascal@codata.org>
2026-03-26 20:48:34 -0600

    feat: add types-ujson to dev dependencies and update mypy check command

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from dotenv import load_dotenv
 
 
-@pytest.fixture(scope="session", autouse=True)  # type: ignore[misc]
+@pytest.fixture(scope="session", autouse=True)
 def load_env() -> None:
     dotenv_path = Path(__file__).parent / "../.env"  # Construct path from current test file dir
     load_dotenv(dotenv_path=dotenv_path)
